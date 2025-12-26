d7a1bc9eb9ea95ab248eac91e2a9faf699eae83b
晴小雨 <xycsj2019@outlook.com>
2025-12-26 11:17:19 +0800

    新建main函数并保留之前的main

---
+++ mpm/CC.h
@@ -0,0 +1,14 @@
+//CC.h 包含所有CommandClass的头文件
+#pragma once
+
+#include "p_mpm.h"
+#include "piwbd.h"
+
+#include "COW.h"
+#include "COP.h"
+#include "CLP.h"
+#include "CLW.h"
+#include "CDP.h"
+#include "CDW.h"
+#include "CDPW.h"
+#include "CDJS.h"
